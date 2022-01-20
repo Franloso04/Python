3e7a786a364fa7e143244864f6991cea0c6e842f
Franloso04 <franloso04@gmail.com>
2022-01-20 09:32:00 +0100

    jueves 20/1

--- HolaMundo.py
+++ HolaMundo.py
@@ -1,6 +1,6 @@
 #Inicio de programa 
 n=int(input("Dime tu edad y te dire tu edad dentro de 10 años"))
-
+n=5
 print("En diez años tendras",n+10)
 
 
@@ -9,5 +9,10 @@ if n>5:
     print("Efectivamente")    
 else:
     print("Es menor que 5")
+
+
+while n==5:
+    print("Repeticion")
+    n+=1 #=que n= n+1
 
 #Fin programa 
