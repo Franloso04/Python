288f836a50609026116a187acd9dfdad0a62fd3d
Franloso04 <franloso04@gmail.com>
2022-02-10 09:28:28 +0100

    Jueves10/2

---
+++ Ejercicios4.0/Ejercicio4.5.py
@@ -0,0 +1,8 @@
+texto= input("Dime una frase")
+contador=0
+for i in texto:
+    if i==" ":
+        contador+=1
+contador+=1
+print(contador)     
+    
---
+++ Ejercicios4.0/Ejercicio4.6.py
@@ -0,0 +1,7 @@
+texto= input("Dime tu nombre completo")
+texto= texto.lower()
+tam=len(texto)
+for i in texto:
+    if i==" ":
+        texto.upper(i)
+print (texto) 
